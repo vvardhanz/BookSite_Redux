4961df409c04c4d184c37988fd796d6fdfb7a9ca
vvardhan <kvvardhanz@gmail.com>
2017-07-24 20:48:09 -0400

    Added book detail container.

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
 
      render() {
+        if (!this.props.book) {
+            return <div> Select a book to get started. </div>
+        }
         return(
            <div> Book  Detail! </div>
 
         );
      }
 }
+
+function mapStateToProps(state) {
+    return {
+       book: state.activeBook
+    };
+}
+
+
+export default connect(mapStateToProps)(BookDetail);
